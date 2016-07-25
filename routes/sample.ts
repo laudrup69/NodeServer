e2b944433bd7585a71b13f25d5ad035441e449c9
Laudrup69 <laudrup69@gmail.com>
2016-07-25 10:42:10 +0200

    bugs en sample

--- routes/sample.ts
+++ routes/sample.ts
@@ -1,11 +1,11 @@
 import * as express from 'express';
-let router = express.router()
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.json({
-        title: 'Hello Express'
-    });
+res.json({
+title: 'Hello Express'
+});
 });
 
 export default router;

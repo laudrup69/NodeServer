aab1aac6fc0bad1ac485124b9707c0d7bdf9a202
Laudrup69 <laudrup69@gmail.com>
2016-07-25 10:28:45 +0200

    app updated

--- app.ts
+++ app.ts
@@ -1,5 +1,3 @@
-///
-
 import * as debugFactory from 'debug';
 import * as http from 'http';
 import * as express from 'express';
@@ -19,24 +17,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(staticsPath, 'public')));
 
-let port = ProcessingInstruction.env.PORT   || 3000;
+let port = process.env.PORT || 3000;
 
-app.set('port, port');
+app.set('port', port);
 
 app.use('/', sampleRoute);
 
-app.use(function (req, res, next) {
-    var err = new Error('Not found');
-    (err).status = 404;
-    next(err);
+app.use(function(req, res, next) {
+var err = new Error('Not Found');
+(err).status = 404;
+next(err);
 });
 
 var server = http.createServer(app);
 server.listen(port);
 server.on('error', (err) =&gt; {
-    console.error(err);
+console.error(err);
 });
 server.on('listening', () =&gt; {
-    var port = server.address().port;
-    debug('Listening on ' + port);
+var port = server.address().port;
+debug('Listening on ' + port);
 });
